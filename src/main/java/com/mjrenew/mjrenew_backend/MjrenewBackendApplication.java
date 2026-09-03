241f723715f3bde6519f93bce33aa83273da9fb1
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-09-03 12:00:28 -0600

    Initial commit en la nueva rama, primera fase del mapeo: vista principal

---
+++ src/main/java/com/mjrenew/mjrenew_backend/MjrenewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mjrenew.mjrenew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjrenewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjrenewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjrenew/mjrenew_backend/MjrenewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjrenew.mjrenew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjrenewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
