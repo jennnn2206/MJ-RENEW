7ab3c319c91c31dd9e4c220b7e7c8a4f6d4a1ee4
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-09-09 11:03:56 -0600

    Refactorizacion inicial de Spring Boot - codigo base

--- src/main/java/com/mjrenew/mjrenew_backend/MjrenewBackendApplication.java
+++ src/main/java/com/mjrenew/mjrenew_backend/MjrenewBackendApplication.java
@@ -2,8 +2,9 @@ package com.mjrenew.mjrenew_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MjrenewBackendApplication {
 
 	public static void main(String[] args) {
